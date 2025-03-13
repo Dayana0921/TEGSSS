8364b6cd655eb02b8d3d0158c6669830a2a58660
Dayana0921 <dayanadayu1432@gmail.com>
2025-03-13 15:25:58 +0530

    first commit

---
+++ TEGSSS/src/main/java/com/Dayana0921/App.java
@@ -0,0 +1,13 @@
+package com.Dayana0921;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
